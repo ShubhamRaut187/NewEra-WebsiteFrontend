eafcdd3fb41d6cde619c250b31e10067ae5b6016
ShubhamRaut187 <raut.shubham4554@gmail.com>
2024-02-18 19:29:03 +0530

    App initialized

---
+++ src/Pages/Home.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import './Page Styles/Home.css'
+function Home(props) {
+    return (
+        <div>
+            
+        </div>
+    );
+}import './Page Styles/Home.css'
+
+export default Home;
---
+++ src/Routes/Navbar.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import './Styles/Navbar.css'
+function Navbar(props) {
+    return (
+        <div>
+            
+        </div>
+    );
+}
+
+export default Navbar;
